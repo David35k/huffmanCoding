22c26af44c79244bcfc98bb2b26a700a63a063ed
David <davidtoman07@gmail.com>
2023-09-12 12:43:39 +1200

    finding unique letters

--- huffmanEncoder.c
+++ huffmanEncoder.c
@@ -1,40 +1,63 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 void checkFreq(char[]);
 
-int main() {
-    
+int main()
+{
+
     char string[100];
-    
+
     printf("input string:");
     fgets(string, 100, stdin);
-    
+
     printf("%s", string);
+
+    checkFreq(string);
 
     return 0;
 }
 
-void checkFreq(char string[]) {
+void checkFreq(char string[])
+{
     char letters[100];
     int freq[100];
-    
-    for(int i = 0;i < 100; i++) {
+
+    for (int i = 0; i < 100; i++)
+    {
         letters[i] = '\0';
         freq[i] = 0;
     }
-    
-    
-    for(int i = 0;i < strlen(string); i++) {
-        // make the array of unique characters
-        // if(string[])
-        
-        // increase the counts
-        for(int j = 0;j < strlen(letters); j++){
-            if(string[i] == letters[j]) {
-                freq[j] += 1;
+
+    // make the array of unique characters
+    for (int i = 0; i < strlen(string); i++)
+    {
+        bool flag = false;
+        for(int j = 0;j<i;j++){
+            if(string[i] == string[j]) {
+                flag = true;
             }
         }
-    }
-}
 
+        if(!flag) {
+            letters[i] = string[i];
+        }
+    }
+
+    for(int i = 0; i < 100; i++) {
+        printf("%c ", letters[i]);
+    }
+
+    // for (int i = 0; i < strlen(string); i++)
+    // {
+    //     // increase the counts
+    //     for (int j = 0; j < strlen(letters); j++)
+    //     {
+    //         if (string[i] == letters[j])
+    //         {
+    //             freq[j] += 1;
+    //         }
+    //     }
+    // }
+}
