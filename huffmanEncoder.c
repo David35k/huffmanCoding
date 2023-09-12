65cb7a6996cfe8263a969dc16d221ca9c42f39dc
David <davidtoman07@gmail.com>
2023-09-12 20:51:36 +1200

    unique letter frequencies

--- huffmanEncoder.c
+++ huffmanEncoder.c
@@ -4,6 +4,9 @@
 
 void checkFreq(char[]);
 
+int frequencies[100];
+char uniqueLetters[100];
+
 int main()
 {
 
@@ -12,7 +15,9 @@ int main()
     printf("input string:");
     fgets(string, 100, stdin);
 
-    printf("%s", string);
+    string[strlen(string) - 1] = '\0';
+
+    printf("%s\n", string);
 
     checkFreq(string);
 
@@ -22,42 +27,71 @@ int main()
 void checkFreq(char string[])
 {
     char letters[100];
-    int freq[100];
+    int freqs[100];
 
     for (int i = 0; i < 100; i++)
     {
-        letters[i] = '\0';
-        freq[i] = 0;
+        letters[i] = '~'; // literally no one uses this character if you use it you are weird
+        freqs[i] = 0;
     }
 
     // make the array of unique characters
     for (int i = 0; i < strlen(string); i++)
     {
         bool flag = false;
-        for(int j = 0;j<i;j++){
-            if(string[i] == string[j]) {
+        // loop through all characters it has already gone over,
+        // if the new character has not been seen before it wouldn't set flag to true
+        for (int j = 0; j < i; j++)
+        {
+            if (string[i] == string[j])
+            {
                 flag = true;
             }
         }
 
-        if(!flag) {
+        // if not flagged it means that the character at i is unique
+        if (!flag)
+        {
+            // add the unique letter to the array of letters
             letters[i] = string[i];
         }
     }
 
-    for(int i = 0; i < 100; i++) {
-        printf("%c ", letters[i]);
+    // check for the frequency of each letter
+    for (int i = 0; i < strlen(string); i++)
+    {
+        // increase the counts if the two letters are the same
+        for (int j = 0; j < strlen(string); j++)
+        {
+            if (string[i] == letters[j])
+            {
+                freqs[j] += 1;
+            }
+        }
     }
 
-    // for (int i = 0; i < strlen(string); i++)
-    // {
-    //     // increase the counts
-    //     for (int j = 0; j < strlen(letters); j++)
-    //     {
-    //         if (string[i] == letters[j])
-    //         {
-    //             freq[j] += 1;
-    //         }
-    //     }
-    // }
+    // assign the frequencies to the global variable
+    for (int i = 0; i < 100; i++)
+    {
+        int count = 0;
+        if (letters[i] != '~' && freqs[i] != 0)
+        {
+            frequencies[i - count] = freqs[i];
+            uniqueLetters[i - count] = letters[i];
+        }
+        else
+        {
+            count++;
+        }
+    }
+
+    // for debugging
+    for (int i = 0; i < 100; i++)
+    {
+        // printf("%d\n", freqs[i]);
+        // if (letters[i] != '~' && freqs[i] != 0)
+        // {
+        printf("\nletter: %c, fredquency: %d", uniqueLetters[i], frequencies[i]);
+        // }
+    }
 }
